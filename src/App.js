3ab646296dfa81ecfe22b572f251a3ff2e498da9
Kazuki Hirota <k.hirota0129@gmail.com>
2021-08-28 15:57:25 +1000

    changed some layout

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import "./App.css";
 
 function App() {
   //defining the state of light (0 or 1)
-  //when the state is 0, left side of the green light is lit
-  //when the state is 1, right side of the green light is lit
-  const [state, setState] = useState(0);
+  const [isRightGreenLit, setIsRightGreenLit] = useState(true);
 
   return (
     <div className="App">
       <h1 className="header">Swap lights</h1>
-      <Lights state={state} />
-      <SwapButton state={state} swapColors={setState} />
+      <Lights state={isRightGreenLit} />
+      <SwapButton state={isRightGreenLit} setState={setIsRightGreenLit} />
     </div>
   );
 }
@@ -22,24 +20,20 @@ const Lights = (props) => {
     <div className="light-row">
       <div className="light-container">
         {/* conditional styling */}
-        <div className={`light-frame ${props.state === 0 ? "" : "dim"}`}>
-          <div className={`light-red ${props.state === 0 ? "lit" : ""}`}></div>
+        <div className={`light-frame ${props.state ? "" : "dim"}`}>
+          <div className={`light-red ${props.state ? "lit" : ""}`}></div>
         </div>
-        <div className={`light-frame ${props.state === 0 ? "dim" : ""}`}>
-          <div
-            className={`light-green ${props.state === 0 ? "" : "lit"}`}
-          ></div>
+        <div className={`light-frame ${props.state ? "dim" : ""}`}>
+          <div className={`light-green ${props.state ? "" : "lit"}`}></div>
         </div>
       </div>
       <div className="light-container">
-        <div className={`light-frame ${props.state === 0 ? "dim" : ""}`}>
-          <div className={`light-red ${props.state === 0 ? "" : "lit"}`}></div>
+        <div className={`light-frame ${props.state ? "dim" : ""}`}>
+          <div className={`light-red ${props.state ? "" : "lit"}`}></div>
         </div>
 
-        <div className={`light-frame ${props.state === 0 ? "" : "dim"}`}>
-          <div
-            className={`light-green ${props.state === 0 ? "lit" : ""}`}
-          ></div>
+        <div className={`light-frame ${props.state ? "" : "dim"}`}>
+          <div className={`light-green ${props.state ? "lit" : ""}`}></div>
         </div>
       </div>
     </div>
@@ -48,7 +42,7 @@ const Lights = (props) => {
 
 const SwapButton = (props) => {
   const swapColors = () => {
-    props.state === 0 ? props.swapColors(1) : props.swapColors(0);
+    props.state ? props.swapColors(false) : props.swapColors(true);
   };
   return (
     <div>
